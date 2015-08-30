1ad39d4b6be573668a07dda4cf4a0efc0b4a9bc6
James Fisher <fisher.jc@gmail.com>
2015-08-30 13:55:40 -0400

    seeded users and teams

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,50 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+I18n.reload!
+
+# Create Users
+5.times do
+  user = User.new(
+    firstname: Faker::Name.first_name,
+    lastname: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(8),
+    role: 'standard' 
+    )
+    user.skip_confirmation!
+    user.save!
+end
+
+# Create Teams
+20.times do
+  team = Team.create(
+    school: Faker::Address.state,
+    nickname: Faker::Team.creature,
+    city: Faker::Address.city,
+    state: Faker::Address.state_abbr,
+    zip: Faker::Address.zip_code,
+    league: "NBA",
+    user: User.all.sample
+    )
+end
+
+# Create an admin
+admin = User.new(
+  email: 'admin@example.com',
+  password: 'helloworld',
+  role: 'admin'
+  )
+admin.skip_confirmation!
+admin.save!
+
+# Create a member
+member = User.new(
+  email: 'member@example.com',
+  password: 'helloworld',
+  role: 'standard'
+  )
+member.skip_confirmation!
+member.save!
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Team.count} teams created"
