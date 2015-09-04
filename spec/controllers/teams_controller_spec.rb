71e9fee06ec34934e79c18a353b300158edb0c1b
James Fisher <fisher.jc@gmail.com>
2015-09-04 16:28:18 -0400

    teams_controller_spec

--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -1,10 +1,12 @@
 require "rails_helper"
 
 describe TeamsController, type: :controller do
+  let(:user) { create(:user)}
+  before {sign_in(user)}
+
   describe "GET index" do
-    subject(:team) { create(:team) }
     it "assigns @teams" do
-      # team = Team.create
+      team = create(:team)
       get :index
       expect(assigns(:teams)).to eq([team])
     end
@@ -15,16 +17,100 @@ describe TeamsController, type: :controller do
     end
   end
 
-  describe "POST create" do
-    # subject(:team) { create(:team) }
-    it "creates a team" do
-      team = post :create, team: attributes_for(:team)
-      expect(team.user_id).to eq(1) 
+  describe "GET show" do
+    let(:team) {create(:team)}
+    it "assigns the requested team to @team" do
+      get :show, id: team
+      expect(assigns(:team)).to eq(team)
+    end
+
+    it "renders the :show template" do
+      get :show, id: team
+      expect(response).to render_template("show")
     end
   end
 
-      # subject(:team) { create(:team) }
-      # create(:team) do |team|
-      #   team.create(attributes_for(:team))
-      # end
+  describe "GET new" do
+    it "assigns a new team as @team" do
+      get :new
+      expect(assigns(:team)).to be_a_new(Team)
+    end
+  end
+
+  describe "POST create" do
+    it "creates a team" do
+      expect{
+        post :create, team: attributes_for(:team)
+        }.to change(Team, :count).by(1) 
+    end
+
+    it "redirects to new team" do
+        post :create, team: attributes_for(:team)
+        expect(response.status).to eq(302)
+    end
+
+    it "assigns team" do
+        post :create, team: attributes_for(:team)
+        expect(assigns(:team)).to be_present
+    end
+  end
+
+  describe "PUT update" do
+    before :each do
+      @team = create(:team)
+    end
+
+    context "valid attributes" do
+      it "located the requested @team" do
+        put :update, id: @team, team: attributes_for(:team)
+        expect(assigns(:team)).to eq(@team)
+      end
+
+      it "changes @team's attributes" do
+        put :update, id: @team, team: attributes_for(:team, school: "Philadelphia")
+        @team.reload
+        expect(@team.school).to eq("Philadelphia")
+      end
+
+      it "redirects to the updated team" do
+        put :update, id: @team, team: attributes_for(:team)
+        expect(response).to redirect_to(@team)
+      end
+    end
+
+    context "invalid attributes" do
+      it "locates the requested @team" do
+        put :update, id: @team, team: attributes_for(:team)
+        expect(assigns(:team)).to_not eq(2)
+      end
+
+      it "does not change @team's attributes" do
+        put :update, id: @team, team: attributes_for(:team, school: nil)
+        @team.reload
+        expect(@team.school).to_not eq("Philadelphia")
+      end
+
+      it "re-renders the edit method" do
+        put :update, id: @team, team: attributes_for(:team, school: nil)
+        expect(response).to redirect_to(@team)
+      end
+    end
+  end
+
+  describe "DELETE destroy" do
+    before :each do
+      @team = create(:team)
+    end
+
+    it "deletes the team" do
+      expect{
+        delete :destroy, id: @team
+      }.to change(Team, :count).by(-1)
+    end
+
+    it "redirects to team#index" do
+      delete :destroy, id: @team
+      expect(response).to redirect_to(teams_path)
+    end
+  end
 end
